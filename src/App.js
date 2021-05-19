bc995c3f37d0e1f42d87a54557f0673be883c773
MarynaKip <54113178+MarynaKip@users.noreply.github.com>
2021-05-19 19:47:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import {useEffect} from 'react';
-impor {useDispatch} from 'react-redux'
 // import { useRoutes } from "hookrouter";
 // import Routes from "./routes";
 import UserMenu from "./components/UserMenu";
@@ -8,10 +7,6 @@ import UserMenu from "./components/UserMenu";
 const App = () => {
   // const routeResult = useRoutes(Routes);
   // return routeResult;
-  const dispatch = useDispatch();
-  useEffect(() => {
-  dispatch();
-  }, [])
 
   return (
     <>
